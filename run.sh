2370585a9909d6d8784eb2e52ab0503c7f63aa1b
Colin Wang <colingwuyu@gmail.com>
2023-08-15 19:42:55 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,2 @@
+python run.py -spread=0.005 -obj_func=var -threshold=0.95 -train_sim=40000 -eval_sim=5000 -critic=qr -std_coef=1.645 -init_vol=0.3 -mu=0.0 -vov=0.0 -vega_obs=False -gbm=True -hed_ttm=30 -liab_ttms=60 -init_ttm=30 -poisson_rate=1.0 -action_space=0,1 -logger_prefix=RL/QR -n_step=5
+python run.py -spread=0.005 -obj_func=var -threshold=0.95 -train_sim=40000 -eval_sim=5000 -critic=gl -std_coef=1.645 -init_vol=0.3 -mu=0.0 -vov=0.0 -vega_obs=False -gbm=True -hed_ttm=30 -liab_ttms=60 -init_ttm=30 -poisson_rate=1.0 -action_space=0,1 -logger_prefix=RL/GL -n_step=5
